0c355f8e2d0811cafe6aab189fa7664bc1c099d1
Andrei243 <andrei243.nma@gmail.com>
2019-03-05 00:32:50 +0200

    new file:   Flash Card.iml 	new file:   Test.fl 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCard.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardBuilder$1.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardBuilder$2.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardBuilder$3.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardBuilder$4.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardBuilder$5.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardBuilder.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardPlayer$1.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardPlayer$2.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardPlayer$3.class 	new file:   out/production/Flash Card/com/Mandel/swing/FlashCardPlayer.class 	new file:   src/com/Mandel/swing/FlashCard.java 	new file:   src/com/Mandel/swing/FlashCardBuilder.java 	new file:   src/com/Mandel/swing/FlashCardPlayer.java

---
+++ src/com/Mandel/swing/FlashCardBuilder.java
@@ -0,0 +1,208 @@
+package com.Mandel.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class FlashCardBuilder {
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<FlashCard> flashCards;
+    private JFrame jFrame;
+
+
+
+
+    private FlashCardBuilder(){
+
+
+        JPanel jPanel=new JPanel();
+        jPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+
+
+
+
+        flashCards=new ArrayList<>();
+        jFrame=new JFrame("Flash Card Create");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(1200,1080);
+        jFrame.setVisible(true);
+        FlowLayout flowLayout=new FlowLayout(FlowLayout.LEFT);
+        Font grit=new Font("Helvetica Neue",Font.BOLD,20);
+
+
+        question=new JTextArea(15,15);
+        question.setFont(grit);
+        question.setText("");
+        question.setLineWrap(true);
+        question.setWrapStyleWord(true);
+
+        answer=new JTextArea(15,15);
+        answer.setFont(grit);
+        answer.setText("");
+        answer.setLineWrap(true);
+        answer.setWrapStyleWord(true);
+
+        jFrame.setLayout(flowLayout);
+
+        JLabel jLabel=new JLabel("Intrebare");
+        JLabel jLabel1=new JLabel("Raspuns");
+
+
+        JScrollPane ajScrollPane=new JScrollPane(answer);
+        ajScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        ajScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+
+        JScrollPane bjScrollPane=new JScrollPane(question);
+        bjScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        bjScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        jPanel.add(jLabel);
+        jPanel.add(bjScrollPane);
+        jPanel.add(jLabel1);
+        jPanel.add(ajScrollPane);
+
+        JButton jButton=new JButton("Adauga FlashCard");
+
+        jButton.setSize(20,70);
+
+        jPanel.add(jButton);
+
+        JLabel jLabel2=new JLabel("");
+        JLabel jLabel3=new JLabel("");
+
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (question.getText().length() == 0) {
+                    jLabel2.setText("Nu ai introdus nicio intrebare");
+                }
+                if (answer.getText().length() == 0) {
+                    jLabel3.setText("Nu ai introdus niciun raspuns");
+                }
+
+                if (question.getText().length() > 0) {
+                    jLabel2.setText("");
+                }
+                if (answer.getText().length() > 0) {
+                    jLabel3.setText("");
+                }
+
+                if (answer.getText().length() > 0 && question.getText().length() > 0) {
+                    FlashCard flashCard = new FlashCard(question.getText(), answer.getText());
+                    flashCards.add(flashCard);
+                    question.setText("");
+                    answer.setText("");
+                    question.requestFocus();
+
+                }
+
+
+            }
+        });
+        jPanel.add(jLabel2);
+        jPanel.add(jLabel3);
+
+
+        JMenuBar jMenuBar=new JMenuBar();
+        JMenu jMenu=new JMenu("File");
+        JMenuItem jMenuItem=new JMenuItem("New");
+        JMenuItem sMenuItem=new JMenuItem("Save");
+        jMenu.add(jMenuItem);
+        jMenu.add(sMenuItem);
+        jMenuBar.add(jMenu);
+
+
+
+        sMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                if (answer.getText().length() > 0 && question.getText().length() > 0) {
+                    FlashCard flashCard = new FlashCard(question.getText(), answer.getText());
+                    flashCards.add(flashCard);
+                    question.setText("");
+                    answer.setText("");
+
+
+                }
+
+
+                JFileChooser savefile = new JFileChooser();
+                savefile.showSaveDialog(jFrame);
+
+                FlashCardBuilder.this.SAVEFILE(savefile.getSelectedFile());
+
+
+            }
+        });
+
+
+        jMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                flashCards.clear();
+            }
+        });
+
+
+        sMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        jFrame.setJMenuBar(jMenuBar);
+        jFrame.getContentPane().add(BorderLayout.CENTER,jPanel);
+
+
+    }
+
+    private void SAVEFILE(File selectedFile) {
+
+        try
+        {
+            BufferedWriter writer=new BufferedWriter(new FileWriter(selectedFile));
+            flashCards.add(new FlashCard("",""));
+
+            for (FlashCard x: flashCards) {
+
+                writer.write(x.getQuestion()+'\\'+x.getAnswer()+'\n');
+
+
+            }
+            flashCards.remove(flashCards.size()-1);
+                writer.close();
+
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("nu s-a putut scrie");
+
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FlashCardBuilder();
+            }
+        });
+    }
+}
---
+++ src/com/Mandel/swing/FlashCardPlayer.java
@@ -0,0 +1,154 @@
+package com.Mandel.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.channels.FileLockInterruptionException;
+import java.util.ArrayList;
+
+public class FlashCardPlayer {
+
+   private ArrayList<FlashCard> flashCards;
+   private boolean este_intrebare;
+   private FlashCard card_actual;
+   private JTextArea jTextArea;
+   private ArrayList<String> texte;
+
+    FlashCardPlayer(){
+        texte=new ArrayList<>();
+        flashCards=new ArrayList<>();
+        jTextArea=new JTextArea(10,15);
+
+
+        JMenuBar jMenuBar=new JMenuBar();
+        JMenu jMenu=new JMenu("File");
+        JMenuItem jMenuItem=new JMenuItem("Open file");
+        jMenu.add(jMenuItem);
+        jMenuBar.add(jMenu);
+
+
+
+        JFrame jframe=new JFrame("Flash Card Viewer");
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setLayout(new FlowLayout(FlowLayout.LEFT));
+        jframe.setSize(1200,1080);
+        jframe.setVisible(true);
+        jframe.setJMenuBar(jMenuBar);
+
+
+        JPanel jPanel=new JPanel();
+        JScrollPane jScrollPane=new JScrollPane(jTextArea);
+        jScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+
+        jPanel.add(jScrollPane);
+
+        JButton jButton=new JButton("Urmatoarea carte");
+
+        jPanel.add(jButton);
+        jframe.add(jPanel);
+
+
+
+
+
+        jMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                JFileChooser jFileChooser=new JFileChooser();
+                jFileChooser.showOpenDialog(jframe);
+                OPENFILE(jFileChooser.getSelectedFile());
+                for(FlashCard flashCard : flashCards ){
+                    System.out.println(flashCard.getQuestion());
+                    texte.add(new String(  flashCard.getQuestion()));
+                    texte.add(new String( flashCard.getAnswer()));
+
+
+                }
+                jTextArea.setText(texte.get(0));
+
+            }
+        });
+
+
+        //int text_actual=1;
+
+        jButton.addActionListener(new ActionListener() {
+            int text_actual=1;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jTextArea.setText(texte.get(text_actual%(texte.size()-2)));
+                text_actual++;
+
+
+            }
+        }
+
+
+        );
+
+
+
+
+
+
+
+    }
+
+    private void OPENFILE(File selectedFile) {
+
+
+        try (FileReader fileReader = new FileReader(selectedFile)) {
+            String aux;
+
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+            do{
+
+                aux=bufferedReader.readLine();
+
+                String aux1,aux2;
+                aux1=aux.substring(0,aux.lastIndexOf("\\"));
+                aux2=aux.substring(aux.lastIndexOf("\\")+1,aux.length());
+                flashCards.add(new FlashCard(aux1,aux2));
+
+
+            }while(!aux.equals("\\"));
+
+            for (FlashCard i: flashCards) {
+                System.out.println(i.getQuestion()+"  "+i.getAnswer());
+
+            }
+
+
+        }
+        catch(Exception e){
+            System.out.println(e.fillInStackTrace());
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                new FlashCardPlayer();
+
+            }
+        });
+
+
+    }
+
+
+
+}
